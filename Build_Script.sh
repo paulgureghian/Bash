e4d36f7b570233580eae380fb65706a2b8714e47
paulgureghian <paulgureghian@att.net>
2020-05-26 15:50:24 -0700

    Created Bash script file

---
+++ Build_Script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+
+# Created by Paul A. Gureghian in May 2020. #
+# This Bash shell script copies certain files from a source directory to a build directory. #
+
+# Start script. #
+
