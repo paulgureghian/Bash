ab094ae748541312ceafd97726ca7af3987c0d8f
paulgureghian <paulgureghian@att.net>
2020-05-26 13:57:38 -0700

    Added a 'while' loop

--- Greeting.sh
+++ Greeting.sh
@@ -12,3 +12,21 @@ later_greeting="How are you"
 greeting_occasion=0
 greeting_limit=$1
 
+# While loop. #
+while [ $greeting_occasion -lt $greeting_limit ]
+
+do
+  
+  if [ $greeting_occasion -lt 1 ]
+  then
+    echo $first_greeting
+
+  else
+    echo $later_greeting
+
+  fi
+  greeting_occasion=$((greeting_occasion + 1))
+
+done
+
+# End of script. #
